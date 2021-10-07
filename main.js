9de0638675e597580d5e5edf97366ca5eba26887
Rodrigo Maia <rodrigo.araujo.maia@g.globo>
2021-10-07 19:50:50 -0300

    2 - Removendo e Acrescentando frutas no array

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ console.log(fruta[0])
 console.log(fruta[1])
 console.log(fruta[2])
 console.log(fruta[3])
+
+console.log(fruta)
+
+fruta.splice(0,1) // Irá remover 1 item na posição 0
+fruta.push("Tamarino") // Acrescentará o Tamarino na última posição
+console.log(fruta)
+
+fruta.splice(0,2) // Irá remover 2 item a partir da posição 0
+fruta.push("Passas", "Tomate") // Acrescentará Passas e Tomate na penúltima e última posição
+console.log(fruta)
